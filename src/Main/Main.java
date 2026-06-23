a03a169c347cb565217dc55ac067768e16285013
TRANBAXUANLAM <xuanlam157205@gmail.com>
2026-06-23 15:45:18 +0700

    First Commit: Initialize Project

---
+++ src/Main/Main.java
@@ -0,0 +1,48 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        boolean[][] seatingMap = new boolean[5][5]; 
+        
+        while (true) {
+            System.out.println("\n--- HỆ THỐNG ĐẶT VÉ XEM PHIM MINI ---");
+            System.out.println("1. Xem danh sách phim và suất chiếu");
+            System.out.println("2. Xem sơ đồ ghế và Đặt vé");
+            System.out.println("3. Hủy đặt vé");
+            System.out.println("4. Tính tổng tiền thanh toán");
+            System.out.println("5. Thoát");
+            System.out.print("Chọn chức năng (1-5): ");
+            
+            int choice = scanner.nextInt();
+            if (choice == 5) {
+                System.out.println("Cảm ơn bạn đã sử dụng dịch vụ!");
+                break;
+            }
+            
+            switch (choice) {
+                case 1:
+                    // Thành viên 1 gọi hàm hiển thị phim ở đây
+                    MovieManager.displayMovies();
+                    break;
+                case 2:
+                    // Thành viên 2 gọi hàm đặt vé ở đây
+                    BookingManager.bookSeat(seatingMap, scanner);
+                    break;
+                case 3:
+                    // Thành viên 2 gọi hàm hủy vé ở đây
+                    BookingManager.cancelSeat(seatingMap, scanner);
+                    break;
+                case 4:
+                    // Thành viên 3 gọi hàm tính tiền ở đây
+                    PaymentManager.calculateTotal(seatingMap);
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ!");
+            }
+        }
+    }
+}
