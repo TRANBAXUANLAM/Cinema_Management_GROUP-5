97459f1f9a33df3a4827009d09596a891951a3d2
phamtiendat <datihinea3ltt@gmail.com>
2026-06-24 21:35:06 +0700

    Thành viên nộp file MovieManager quản lý lịch chiếu phim.

---
+++ src/Main/MovieManager.java
@@ -0,0 +1,20 @@
+
+package Main;
+
+public class MovieManager {
+    
+    public static void displayMovies() {
+        System.out.println("\n=============================================");
+        System.out.println(" DANH SÁCH PHIM ĐANG CHIẾU TẠI RẠP MINI ");
+        System.out.println("=============================================");
+        System.out.println("1. Doraemon: Bản Tình Ca Vũ Trụ");
+        System.out.println("   [Suất chiếu: 14:00] | [Giá vé: 70.000 VND]");
+        System.out.println("---------------------------------------------");
+        System.out.println("2. Lật Mặt 7: Một Điều Ước");
+        System.out.println("   [Suất chiếu: 17:30] | [Giá vé: 80.000 VND]");
+        System.out.println("---------------------------------------------");
+        System.out.println("3. Ma Da");
+        System.out.println("   [Suất chiếu: 20:00] | [Giá vé: 90.000 VND]");
+        System.out.println("=============================================");
+    }
+}
